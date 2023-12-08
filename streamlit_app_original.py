803b36ffa3d80cf0164d05d04d312bd1bc935bef
ANDRE GARCIA <osvaldoag@gmail.com>
2023-12-08 19:31:14 +0100

    second try

--- streamlit_app_original.py
+++ streamlit_app_original.py
@@ -1,9 +1,7 @@
-# import altair as alt
-# import numpy as np
-# import pandas as pd
+import altair as alt
+import numpy as np
+import pandas as pd
 import streamlit as st
-from gradio_client import Client
-
 
 """
 # Welcome to Streamlit!
@@ -14,60 +12,29 @@ forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
-#
-# num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-# num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
-#
-# indices = np.linspace(0, 1, num_points)
-# theta = 2 * np.pi * num_turns * indices
-# radius = indices
-#
-# x = radius * np.cos(theta)
-# y = radius * np.sin(theta)
-#
-# ## example from internet
-# import requests
-# import streamlit as st
-#
-# data = requests.get("'https://jsonplaceholder.typicode.com/todos/1'").json()
-#
-# st.write(data)
-# ## end
-#
 
+num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
+num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
 
+indices = np.linspace(0, 1, num_points)
+theta = 2 * np.pi * num_turns * indices
+radius = indices
 
-client = Client("https://andre-garcia-rest-api-alpha.hf.space/--replicas/chhgt/")
-result = client.predict(
-		"WUSUUUUUUUUUUUUUUUP!!",	# str  in 'name' Textbox component
-							api_name="/predict"
-)
-print(result)
-st.write(result)
+x = radius * np.cos(theta)
+y = radius * np.sin(theta)
 
+df = pd.DataFrame({
+    "x": x,
+    "y": y,
+    "idx": indices,
+    "rand": np.random.randn(num_points),
+})
 
-
-
-
-
-
-#
-#
-# df = pd.DataFrame({
-#     "x": x,
-#     "y": y,
-#     "idx": indices,
-#     "rand": np.random.randn(num_points),
-# })
-#
-
-#
-#
-# st.altair_chart(alt.Chart(df, height=700, width=700)
-#     .mark_point(filled=True)
-#     .encode(
-#         x=alt.X("x", axis=None),
-#         y=alt.Y("y", axis=None),
-#         color=alt.Color("idx", legend=None, scale=alt.Scale()),
-#         size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-#     ))
+st.altair_chart(alt.Chart(df, height=700, width=700)
+    .mark_point(filled=True)
+    .encode(
+        x=alt.X("x", axis=None),
+        y=alt.Y("y", axis=None),
+        color=alt.Color("idx", legend=None, scale=alt.Scale()),
+        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
+    ))
